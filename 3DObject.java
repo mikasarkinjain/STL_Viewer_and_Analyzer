df8a89d7adda27bddd942cd04ad1699fe17d8049
Ida <ihuangda@gmail.com>
2014-01-12 22:19:55 -0500

    3D Object class

---
+++ 3DObject.java
@@ -0,0 +1,27 @@
+// Team Double A
+// Period 8
+// STL Viewer and Analyzer
+
+public class 3DObject{
+
+    private ArrayList<Triangle> triangles = new ArrayList<Triangle>();
+    
+    public 3DObject (ArrayList<Triangle> t) {
+	for (Triangle i: t) {
+	    triangles.add(i);
+	}
+    }
+
+     public String toString() {
+	 return triangles; 
+     }
+
+    public Triangle getTriangles(int n) {
+	return triangles.get(n-1); 
+    }
+
+    public Triangle projectTriangle(int n) {
+	Triangle projected = new Triangle((triangle.get(n-1)).projectedCor(1), (triangle.get(n-1)).projectedCor(2),(triangle.get(n-1)).projectedCor(3));
+	return projected;
+    }
+}
