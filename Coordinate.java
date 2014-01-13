e3a87294972f0d3d3f726153df37b87386f0dcc1
Ida <ihuangda@gmail.com>
2014-01-12 22:14:20 -0500

    Coordinate class

---
+++ Coordinate.java
@@ -0,0 +1,36 @@
+// Team Double A
+// Period 8
+// STL Viewer and Analyzer
+
+public class Coordinate {
+
+    private double[] coordinate = new double[3];
+    
+    public Coordinate(double xcor, double ycor, double zcor ) {
+	coordinate[0] = xcor;
+	coordinate[1] = ycor;
+	coordinate[2] = zcor;
+    }
+
+    public String toString() {
+	String ans= "[";
+	for (double i : coordinate) {
+	    ans += i + " "; 
+	}
+	ans += "]";
+	return ans;
+    }
+
+    public double[] getCor() {
+	return coordinate; 
+    }
+
+    public double[] projectCor() {
+	double[] ans = new double[3];
+	ans[0] = coordinate[0]; //xcor
+	ans[1] = coordinate[1]; //ycor
+	// projecting math here:
+	// ans[2] = ?
+	return ans;
+    }
+}
