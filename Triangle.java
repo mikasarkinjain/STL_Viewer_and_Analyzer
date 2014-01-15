cd7468366ec831e411c5dcf30115e85ab37fcdba
Ida <ihuangda@gmail.com>
2014-01-14 20:34:17 -0500

    fixed bugs

--- Triangle.java
+++ Triangle.java
@@ -2,30 +2,42 @@
 // Period 8
 // STL Viewer and Analyzer 
 
+//ArrayList's implementation is in the java.util package
+import java.util.ArrayList;
+
 public class Triangle {
 
-    private ArrayList<Coordinate> coordinates = new ArrayList<Coordinate>(); //HAS TO BE ARRAY OF THE OBJECT COORDINATE
+    // instance of class ArrayList, holding objects of type Coordinate
+    private ArrayList<Coordinate> coordinates; 
     
+    //default constructor initializes instance variables coordinate
     public Triangle (double[][] cors) {
+	coordinates = new ArrayList <Coordinate>();
+
+	Coordinate in;
     	for (double[] i: cors){
-    		Coordinate in = new Coordinate(i);
-		coordinates.add(in);//adds a double[] = Coordinate // You have to instantiate a coordinate to satisfy this ^^
+	    in = new Coordinate(i);
+	    coordinates.add(in);//adds a double[] = Coordinate 
+	    // You have to instantiate a coordinate to satisfy this ^^
     	}
     }
 
     public String toString() {
-	return coordinates;
+	return coordinates.toString();
     }
 
     public Coordinate getCor(int n) {
 	return coordinates.get(n); 
     }
 
-    public double[][] getProjectedTriangle(double xTheta, double yTheta, double zTheta) {
+    //public double[][] getProjectedTriangle(double xTheta, double yTheta, double zTheta) {
+    public double[][] getProjectedTriangle() {
     	double[][] projectedCoords = new double[3][2];
-    	projectedCoords[0] = coordinates.get(0).getProjectedCoord(xTheta, yTheta, zTheta);
-    	projectedCoords[1] = coordinates.get(1).getProjectedCoord(xTheta, yTheta, zTheta);
-    	projectedCoords[2] = coordinates.get(2).getProjectedCoord(xTheta, yTheta, zTheta);
+  	//projectedCoords[0] = coordinates.get(0).getProjectedCoord( xTheta, yTheta, zTheta);
+	//code above doesn't work because the getProjectedCoord takes no params
+    	projectedCoords[0] = coordinates.get(0).getProjectedCoord();
+    	projectedCoords[1] = coordinates.get(1).getProjectedCoord();
+    	projectedCoords[2] = coordinates.get(2).getProjectedCoord();
 	return projectedCoords;
     }
 }
