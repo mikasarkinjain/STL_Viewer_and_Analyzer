5862b75ed67e6cfb5715d7f3959425d8fe9b7733
Ida <ihuangda@gmail.com>
2014-01-14 20:36:34 -0500

    Driver to test Coordinate, Triangle, and ThreeDObject classes IH

---
+++ TestDriver.java
@@ -0,0 +1,54 @@
+// Team Double A
+// Period 8
+// STL Viewer and Analyzer
+
+public class TestDriver {
+
+    public static void main ( String[] args ) {
+    
+	//Testing Coordinate Class
+	System.out.println("TESTING COORDINATE CLASS \n");
+	double[] x = { 0.0, 1.0, 2.0 };
+	Coordinate c1 = new Coordinate( x );
+	System.out.println("Coordinates of c1: \n" + c1); //test toString
+	double [] get = c1.getCor(); //test getCor
+	Coordinate getc1 = new Coordinate( get );
+	System.out.println("Getting the coordinates of c1: \n" + getc1);
+	double[] p = c1.getProjectedCoord(); //test getProjectedCoord
+	System.out.println("Projected xcor of c1: " + p[0] );
+	System.out.println("Projected ycor of c1: " + p[1] );
+
+	//Testing Triangle Class
+	System.out.println("\nTESTING TRIANGLE CLASS \n");
+	double[][] x2 = {{0.0, 1.0, 2.0} , {1.0, 2.0, 3.0} , {2.0, 3.0, 4.0} };
+	Triangle t1 = new Triangle(x2);
+	System.out.println("Printing Coordinates of t1: \n" + t1); //test toString
+	//testing getCor (1,2,3)
+	System.out.println("first coordinate of t1: \n" + t1.getCor(0));
+	System.out.println("second coordinate of t1: \n" + t1.getCor(1));
+	System.out.println("third coordinate of t1: \n" + t1.getCor(2));
+	//testing getProjectedTriangle
+	double[][] pt =  t1.getProjectedTriangle();
+	Triangle projectedt = new Triangle(pt);
+	System.out.println("projected t1: " + projectedt );
+	
+	// Testing 3D Object Class
+	System.out.println("\nTESTING 3DOBJECTCLASS \n");
+	double[][][] x3 = { { {1.0, 1.0, 1.0} , {1.0, 1.0, 1.0} , {1.0, 1.0, 1.0} },
+			    { {2.0, 2.0, 2.0} , {2.0, 2.0, 2.0} , {2.0, 2.0, 2.0} },
+			    { {3.0, 3.0, 3.0} , {3.0, 3.0, 3.0} , {3.0, 3.0, 3.0} } };	
+	ThreeDObject o1 = new ThreeDObject(x3);//testing toString
+	System.out.println("Printing 3DObject of o1: \n" + o1); //testing toString
+	//testing getTriangle
+	System.out.println("Triangles in This Object:");
+	for (int i = 0; i < 3; i++) {
+	    System.out.println("Triangle " + i + ": " + o1.getTriangle(i));
+	}
+	//testing getProjected3DObject 
+	double[][][] pdo = o1.getProjected3DObject();
+	ThreeDObject projectedo = new ThreeDObject(pdo);
+	System.out.println("Projected o1: " + projectedo );
+    
+    }// end main method
+ 
+}// end TestDriver
