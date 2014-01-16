5974a86dc4544e089a95ab049d73e701215795b9
Ida <ihuangda@gmail.com>
2014-01-15 19:40:29 -0500

    Fixed Projected methods to include Theta and now also test FileData class IH

--- TestDriver.java
+++ TestDriver.java
@@ -6,6 +6,7 @@ public class TestDriver {
 
     public static void main ( String[] args ) {
     
+	
 	//Testing Coordinate Class
 	System.out.println("TESTING COORDINATE CLASS \n");
 	double[] x = { 0.0, 1.0, 2.0 };
@@ -22,22 +23,24 @@ public class TestDriver {
 	System.out.println("\nTESTING TRIANGLE CLASS \n");
 	double[][] x2 = {{0.0, 1.0, 2.0} , {1.0, 2.0, 3.0} , {2.0, 3.0, 4.0} };
 	Triangle t1 = new Triangle(x2);
-	System.out.println("Printing Coordinates of t1: \n" + t1); //test toString
+	System.out.println("Printing Coordinates of t1: \n" + t1); 
+	//test toString
+
 	//testing getCor (1,2,3)
 	System.out.println("first coordinate of t1: \n" + t1.getCor(0));
 	System.out.println("second coordinate of t1: \n" + t1.getCor(1));
 	System.out.println("third coordinate of t1: \n" + t1.getCor(2));
 	//testing getProjectedTriangle
-	double[][] pt =  t1.getProjectedTriangle();
+	double[][] pt =  t1.getProjectedTriangle(0.0, 0.0, 0.0);
 	Triangle projectedt = new Triangle(pt);
 	System.out.println("projected t1: " + projectedt );
 	
-	// Testing 3D Object Class
-	System.out.println("\nTESTING 3DOBJECTCLASS \n");
+	// Testing TriangleObject Class
+	System.out.println("\nTESTING TRIANGLEOBJECT CLASS \n");
 	double[][][] x3 = { { {1.0, 1.0, 1.0} , {1.0, 1.0, 1.0} , {1.0, 1.0, 1.0} },
 			    { {2.0, 2.0, 2.0} , {2.0, 2.0, 2.0} , {2.0, 2.0, 2.0} },
 			    { {3.0, 3.0, 3.0} , {3.0, 3.0, 3.0} , {3.0, 3.0, 3.0} } };	
-	ThreeDObject o1 = new ThreeDObject(x3);//testing toString
+	TriangleObject o1 = new TriangleObject(x3);//testing toString
 	System.out.println("Printing 3DObject of o1: \n" + o1); //testing toString
 	//testing getTriangle
 	System.out.println("Triangles in This Object:");
@@ -45,10 +48,15 @@ public class TestDriver {
 	    System.out.println("Triangle " + i + ": " + o1.getTriangle(i));
 	}
 	//testing getProjected3DObject 
-	double[][][] pdo = o1.getProjected3DObject();
-	ThreeDObject projectedo = new ThreeDObject(pdo);
+	double[][][] pdo = o1.getProjected3DObject(0.0,0.0,0.0);
+	TriangleObject projectedo = new TriangleObject(pdo);
 	System.out.println("Projected o1: " + projectedo );
-    
+       
+	//Testing FileData class
+	System.out.println("\nTESTING FILEDATA CLASS \n");
+	FileData f1 = new FileData("samplecube.stl");
+	System.out.println((f1.getFileData())[0][0][0]);
+	
     }// end main method
  
 }// end TestDriver
