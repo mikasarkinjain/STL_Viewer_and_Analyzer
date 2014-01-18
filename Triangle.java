0b1ae2a46fe5687d60786c8a139eab5a3780a660
mikasarkinjain <mikasarkinjain@gmail.com>
2014-01-17 22:46:54 -0500

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -10,7 +10,7 @@ public class Triangle {
     // instance of class ArrayList, holding objects of type Coordinate
     private ArrayList<Coordinate> coordinates; 
     
-    //constructor initializes instance variables coordinates
+    //default constructor initializes instance variables coordinate
     public Triangle (double[][] cors) {
 	coordinates = new ArrayList <Coordinate>();
 
@@ -21,47 +21,45 @@ public class Triangle {
 	    // You have to instantiate a coordinate to satisfy this ^^
     	}
     }
-   
-    //Returns a certain coordinate(x,y,z) of triangle 
-    public Coordinate getCor(int n) {
-	return coordinates.get(n); 
-    }
-    
-    //toString method to print the array all all coordinates that make up this triangle
+
     public String toString() {
 	return coordinates.toString();
     }
-    
-    //Returns the max x, y, z coordinate of each triangle
+
+    public Coordinate getCor(int n) {
+	return coordinates.get(n); 
+    }
+
     public double[] maxCor() {
-	double[] max = new double[3];
-	for (int i = 0; i < 3; i++) {
-	    double a = (getCor(0)).getCor(i);
-	    double b = (getCor(1)).getCor(i);
-	    double c = (getCor(2)).getCor(i);
-	    max[i] = Math.max(c, Math.max(a,b));  
-	}
-	return max;
+        double[] max = new double[3];
+        for (int i = 0; i < 3; i++) {
+            double a = (getCor(0)).getCor(i);
+            double b = (getCor(1)).getCor(i);
+            double c = (getCor(2)).getCor(i);
+            max[i] = Math.max(c, Math.max(a,b));  
+        }
+        return max;
     }
 
     //Returns the min x, y, z coordinate of each triangle
     public double[] minCor() {
-	double[] min = new double[3];
-	for (int i = 0; i < 3; i++) {
-	    double a = (getCor(0)).getCor(i);
-	    double b = (getCor(1)).getCor(i);
-	    double c = (getCor(2)).getCor(i);
-	    min[i] = Math.min(c, Math.min(a,b));  
-	}
-	return min;
+        double[] min = new double[3];
+        for (int i = 0; i < 3; i++) {
+            double a = (getCor(0)).getCor(i);
+            double b = (getCor(1)).getCor(i);
+            double c = (getCor(2)).getCor(i);
+            min[i] = Math.min(c, Math.min(a,b));  
+        }
+        return min;
     }
-    
-    public double[][] getProjectedTriangle(double xTheta, double yTheta, double zTheta) {
-    	double[][] projectedCoords = new double[3][2];
-    	projectedCoords[0] = coordinates.get(0).getProjectedCoord(xTheta, yTheta, zTheta);
-    	projectedCoords[1] = coordinates.get(1).getProjectedCoord(xTheta, yTheta, zTheta);
-    	projectedCoords[2] = coordinates.get(2).getProjectedCoord(xTheta, yTheta, zTheta);
 
+    public double[][] getProjectedTriangle(double xTheta, double yTheta, double zTheta, double zoom, double[] max, double[] min) {
+    	double[][] projectedCoords = new double[3][2];
+  	//projectedCoords[0] = coordinates.get(0).getProjectedCoord( xTheta, yTheta, zTheta);
+	//code above doesn't work because the getProjectedCoord takes no params
+            projectedCoords[0] = coordinates.get(0).getProjectedCoord(xTheta, yTheta, zTheta, zoom, max, min);
+            projectedCoords[1] = coordinates.get(1).getProjectedCoord(xTheta, yTheta, zTheta, zoom, max, min);
+            projectedCoords[2] = coordinates.get(2).getProjectedCoord(xTheta, yTheta, zTheta, zoom, max, min);
 	return projectedCoords;
     }
 }
