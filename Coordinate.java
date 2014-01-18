5836ec2b38c3089de1cc621ee2acaa4f429b3aba
mikasarkinjain <mikasarkinjain@gmail.com>
2014-01-17 22:46:15 -0500

    Update Coordinate.java

--- Coordinate.java
+++ Coordinate.java
@@ -6,50 +6,57 @@ public class Coordinate {
 
     private double[] coordinate = new double[3];
     
-    //constructor initializes instance variables in coordinate
     public Coordinate(double[] cors) {
-	for (int i = 0; i < cors.length; i++){
-	    coordinate[i] = cors[i];	
-	}
+        for (int i = 0; i < cors.length; i++){
+                coordinate[i] = cors[i];        
+        }
     }
 
-    // toString method to print the array of coordinates
     public String toString() {
-	String ans= "[";
-	for (double i : coordinate) {
-	    ans += i + ", "; 
-	}
-	ans = ans.substring(0, ans.length() - 2);
-	ans += "]";
-	return ans;
+        String ans= "[";
+        for (double i : coordinate) {
+            ans += i + ", "; 
+        }
+        ans = ans.substring(0, ans.length() - 2);
+        ans += "]";
+        return ans;
     }
 
-    //Method to get all coordinates (x, y, and z)
     public double[] getCor() {
-	return coordinate; 
+        return coordinate; 
     }
 
-
-    //Method to get a certain coordinate(x, y, or z)
     public double getCor(int n) {
-	return coordinate[n];
+        return coordinate[n];
     }
 
-	public double[] getProjectedCoord(double xTheta, double yTheta, double zTheta) {
-	
-	double x2 = Math.pow(Math.pow(coordinate[0],2)+Math.pow(coordinate[1],2), 0.5) * Math.sin(zTheta + Math.atan(coordinate[1]/coordinate[0])); 
-	double y2 = Math.pow(Math.pow(coordinate[0],2)+Math.pow(coordinate[1],2), 0.5) * Math.cos(zTheta + Math.atan(coordinate[1]/coordinate[0]));
-	
-	double x3 = Math.pow(Math.pow(x2,2)+Math.pow(coordinate[2],2), 0.5) * Math.sin(yTheta + Math.atan(coordinate[2]/x2)); 
-	double z3 = Math.pow(Math.pow(x2,2)+Math.pow(coordinate[2],2), 0.5) * Math.cos(yTheta + Math.atan(coordinate[2]/x2));
-	
-	double y4 = Math.pow(Math.pow(y2,2)+Math.pow(z3,2), 0.5) * Math.sin(xTheta + Math.atan(z3/y2)); 
-	double z4 = Math.pow(Math.pow(y2,2)+Math.pow(z3,2), 0.5) * Math.cos(xTheta + Math.atan(z3/y2));
+    public double[] getProjectedCoord(double xTheta, double yTheta, double zTheta, double zoom, double[] max, double[] min) {
+        double[] coordinate2 = new double[3]; 
+        coordinate2[0] = coordinate[0];// - (max[0] - min[0]) / 2;
+        coordinate2[1] = coordinate[1];// - (max[1] - min[1]) / 2;
+        coordinate2[2] = coordinate[2];// - (max[2] - min[2]) / 2;
 
-	double[] ans = new double[2];
-	ans[0] = x3; //xcor
-	ans[1] = y4; //ycor
-	
-	return ans;
+        double x = Math.pow(Math.abs(Math.pow(coordinate2[0],2)+Math.pow(coordinate2[1],2)), 0.5) * Math.sin(zTheta + Math.atan((coordinate2[1]+0.000000000000000314)/coordinate2[0]+0.000000000000000314)); 
+        double y = Math.pow(Math.abs(Math.pow(coordinate2[0],2)+Math.pow(coordinate2[1],2)), 0.5) * Math.cos(zTheta + Math.atan((coordinate2[1]+0.000000000000000314)/coordinate2[0]+0.000000000000000314));
+        
+        x = x;
+        y = y;
+        double z = coordinate2[2];
+
+        y = Math.pow(Math.abs(Math.pow(y,2)+Math.pow(z,2)), 0.5) * Math.sin(yTheta + Math.atan((y+0.000000000000000314)/z+0.000000000000000314)); 
+        z = Math.pow(Math.abs(Math.pow(y,2)+Math.pow(z,2)), 0.5) * Math.cos(yTheta + Math.atan((y+0.000000000000000314)/z+0.000000000000000314));
+        
+        
+
+        /*
+        double y4 = Math.pow(Math.abs(Math.pow(y2,2)+Math.pow(z3,2)), 0.5) * Math.sin(xTheta + Math.atan((z3+0.000000000000000314)/y2+0.000000000000000314)); 
+        double z4 = Math.pow(Math.abs(Math.pow(y2,2)+Math.pow(z3,2)), 0.5) * Math.cos(xTheta + Math.atan((z3+0.000000000000000314)/y2+0.000000000000000314));
+        */
+
+        double[] ans = new double[2];
+        ans[0] = x * 0.5 * zoom; //xcor
+        ans[1] = y * 0.5 * zoom; //ycor
+        
+        return ans;
     }
 }
