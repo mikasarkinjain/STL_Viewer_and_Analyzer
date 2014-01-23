cc96e230fe8a7f7dee16d6d43cf5537fba7fe661
ihuangda <ihuangda@users.noreply.github.com>
2014-01-23 13:54:43 -0500

    Added comments

--- TriangleObject.java
+++ TriangleObject.java
@@ -7,8 +7,17 @@ import java.util.ArrayList;
 
 public class TriangleObject {
 
+    // instance of class ArrayList, holding objects of type Triangle
+    // ArrayList triangles: { { Triangle1 } , { Triangle2 }, ... ,{ TriangleN } } 
+    // 			    { { { x1,y1,z1 } , { x2, y2, z2 }, { x3, y3, z3 } }, ...
+    //	  		     { { x1,y1,z1 } , { x2, y2, z2 }, { x3, y3, z3 } } } 
     private ArrayList<Triangle> triangles = new ArrayList<Triangle>(); //HAS TO BE ARRAY OF TRIANGLE OBJECTS
     
+   /*==================================================
+     constructor -- initializes a TriangleObject object
+     pre:
+     post: adds Triangles objects to ArrayList triangles
+     ==================================================*/
     public TriangleObject (double[][][] t) {
 	for (double[][] i: t) {
             Triangle in = new Triangle(i);
@@ -16,14 +25,33 @@ public class TriangleObject {
 	}
     }
 
+
+   /*==================================================
+     String toString() -- iterates through the ArrayList coordinates
+     post: to print out the coordinate like this: [ [[xcor1, ycor1, zcor1],[xcor2, ycor2, zcor2],[xcor3, ycor3,zcor3]],
+     						    [[xcor1, ycor1, zcor1],[xcor2, ycor2, zcor2],[xcor3, ycor3,zcor3]],
+     						    ....]
+     ==================================================*/ 
     public String toString() {
 	return triangles.toString(); 
     }
 
+
+   /*==================================================
+     Accessor method: Triangle getTriangle(int n)
+     post: returns the nth Triangle in ArrayList triangles
+     ==================================================*/
     public Triangle getTriangle(int n) {
 	return triangles.get(n); 
     }
 
+
+  /*==================================================
+    double[] getMax() -- iterates through the ArrayList triangles
+    to find the maximum xcor, ycor, and zcor of all Triangles
+    pre:
+    post: returns double[] containing the max xcor, ycor and zcor
+    ==================================================*/
     public double[] getMax(){
         double[] max = new double[3];
         for (Triangle i : triangles) {
@@ -34,8 +62,14 @@ public class TriangleObject {
         }
         return max;
     }
-
-    //getMin returns a 1D array in the form [x,y,z] of the min cors 
+    
+    
+  /*==================================================
+    double[] getMin() -- iterates through the ArrayList triangles
+    to find the minimum xcor, ycor, and zcor of all Triangles
+    pre:
+    post: returns double[] containing the min xcor, ycor and zcor
+    ==================================================*/
     public double[] getMin(){
         double[] min = new double[3];
         for (Triangle i : triangles) {
@@ -47,11 +81,17 @@ public class TriangleObject {
         return min;
     }
 
+ /*==================================================
+   double[] getProjectedTriangle(double, double, double, double)
+   pre:
+   post: returns double[][][] of projected TriangleObject
+   ==================================================*/
     public double[][][] getProjected3DObject(double xTheta, double yTheta, double zTheta, double zoom) {
     	double[][][] projected3DObject = new double[triangles.size()][3][2];
     	for (int i = 0; i < triangles.size(); i++){
 	    projected3DObject[i] = triangles.get(i).getProjectedTriangle(xTheta, yTheta, zTheta, zoom, getMax(), getMin());
     	}
     	return projected3DObject;
-    }
-}
+    }// end getProjected3DObject method
+
+} // end TriangleObject class
