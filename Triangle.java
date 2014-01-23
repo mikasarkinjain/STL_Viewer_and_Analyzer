3419db2948963c9c1e62adfed6e24132d959cc7e
ihuangda <ihuangda@users.noreply.github.com>
2014-01-23 13:38:31 -0500

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -8,9 +8,15 @@ import java.util.ArrayList;
 public class Triangle {
 
     // instance of class ArrayList, holding objects of type Coordinate
+    // ArrayList coordinates looks like:  { {x1,y1,z1} , {x2, y2, z2}, ... , {xn, yn, zn} }  
     private ArrayList<Coordinate> coordinates; 
     
-    //default constructor initializes instance variables coordinate
+    
+    /*==================================================
+      constructor -- initializes a Triangle object
+      pre:
+      post: adds coordinate values to the coordinate array
+      ==================================================*/
     public Triangle (double[][] cors) {
 	coordinates = new ArrayList <Coordinate>();
 
@@ -22,14 +28,31 @@ public class Triangle {
     	}
     }
 
+
+   /*==================================================
+     String toString() -- iterates through the ArrayList coordinates
+     post: to print out the coordinate like this: [[xcor1, ycor1, zcor1],[xcor2, ycor2, zcor2],[xcor3, ycor3, zcor3]]
+     ==================================================*/ 
     public String toString() {
 	return coordinates.toString();
     }
-
+   
+   
+    /*==================================================
+      Accessor method: Coordinate getCor(int n) 
+      post: returns the nth Coordinate in ArrayList coordinates
+      ==================================================*/
     public Coordinate getCor(int n) {
 	return coordinates.get(n); 
     }
+    
 
+   /*==================================================
+     double[] maxCor() -- iterates through the ArrayList Coordinates 
+     to find the maximum xcor, ycor, and zcor of all Coordinates
+     pre:
+     post: returns double[] containing the max xcor, ycor and zcor
+     ==================================================*/
     public double[] maxCor() {
         double[] max = new double[3];
         for (int i = 0; i < 3; i++) {
@@ -40,8 +63,14 @@ public class Triangle {
         }
         return max;
     }
-
-    //Returns the min x, y, z coordinate of each triangle
+    
+    
+   /*==================================================
+     double[] minCor() -- iterates through the ArrayList Coordinates 
+     to find the miniumum xcor, ycor, and zcor of all Coordinates
+     pre:
+     post: returns double[] containing the min xcor, ycor and zcor
+     ==================================================*/
     public double[] minCor() {
         double[] min = new double[3];
         for (int i = 0; i < 3; i++) {
@@ -52,14 +81,21 @@ public class Triangle {
         }
         return min;
     }
-
+    
+    
+  /*==================================================
+    double[] getProjectedTriangle(double, double, double, double, double[], double[])
+    pre:
+    post: returns double[][] of projected Triangle 
+    ==================================================*/
     public double[][] getProjectedTriangle(double xTheta, double yTheta, double zTheta, double zoom, double[] max, double[] min) {
     	double[][] projectedCoords = new double[3][2];
-  	//projectedCoords[0] = coordinates.get(0).getProjectedCoord( xTheta, yTheta, zTheta);
-	//code above doesn't work because the getProjectedCoord takes no params
+
             projectedCoords[0] = coordinates.get(0).getProjectedCoord(xTheta, yTheta, zTheta, zoom, max, min);
             projectedCoords[1] = coordinates.get(1).getProjectedCoord(xTheta, yTheta, zTheta, zoom, max, min);
             projectedCoords[2] = coordinates.get(2).getProjectedCoord(xTheta, yTheta, zTheta, zoom, max, min);
+            
 	return projectedCoords;
-    }
-}
+    } // end getProjectTriangle method
+    
+} // end Triangle class
